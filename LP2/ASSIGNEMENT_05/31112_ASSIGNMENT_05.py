5b23a02b8e8513acc35835ee9278088084505ed3
Navin Bhandari <62202376+mean-understanding45@users.noreply.github.com>
2022-05-15 16:15:48 +0530

    Update 31112_ASSIGNMENT_05.py

--- LP2/ASSIGNEMENT_05/31112_ASSIGNMENT_05.py
+++ LP2/ASSIGNEMENT_05/31112_ASSIGNMENT_05.py
@@ -1,94 +1,88 @@
-# Develop an elementary chatboat for any suitable customer interaction application.
+from nltk.chat.util import Chat, reflections
 
-# Banking Customer Service
-# User can query about loan details, FD details, account opening and other procedures
+pairs = [
+    [
+        r"my name is (.*)",
+        ["Hello %1, How are you today ?",]
+    ],
+     [
+        r"what is your name ?",
+        ["My name is Chatty and I'm a chatbot ?",]
+    ],
+    [
+        r"how are you ?",
+        ["I'm doing good\nHow about You ?",]
+    ],
+    [
+        r"sorry (.*)",
+        ["Its alright","Its OK, never mind",]
+    ],
+    [
+        r"i'm (.*) doing good",
+        ["Nice to hear that","Alright :)",]
+    ],
+    [
+        r"hi|hey|hello",
+        ["Hello", "Hey there",]
+    ],
+    [
+        r"(.*) age?",
+        ["I'm a computer program dude\nSeriously you are asking me this?",]
 
-import random
-print("\nWelcome to the cupcakes Shop!!!\n")
-print("BOT: What do you want me to call you?")
-user_name = input()
-print("Hii", (user_name),"!!")
+    ],
+    [
+        r"what (.*) want ?",
+        ["Make me an offer I can't refuse",]
 
-bot_template = "BOT : {0}"
-user_template = user_name + " : {0}"
+    ],
+    [
+        r"(.*) created ?",
+        ["Vedant created me using Python's NLTK library ","top secret ;)",]
+    ],
+    [
+        r"(.*) (location|city) ?",
+        ['Chennai, Tamil Nadu',]
+    ],
+    [
+        r"how is weather in (.*)?",
+        ["Weather in %1 is awesome like always","Too hot man here in %1","Too cold man here in %1","Never even heard about %1"]
+    ],
+    [
+        r"i work in (.*)?",
+        ["%1 is an Amazing company, I have heard about it. But they are in huge loss these days.",]
+    ],
 
-name = "Cupcakes shop Assistant" 
-flavours= "Mango, Chocolate, vanilla " 
-price = "Mango-50, Chocolate-70, Vanilla-60 (per piece)"
-time = "10 AM to 9 PM"
-bye = "Have a nice day!"
-
-responses = { 
-"what's your name?": [ "They call me {0}".format(name), "I usually go by {0}".format(name), 
-"My name is the {0}".format(name) ],
-
-"what's today's available flavours?": [ 
-"The available flavours are {0}! Grab your favourite!".format(flavours), 
-"It's {0} !!".format(flavours), 
-"Let me check, the flavours available today are {0}!".format(flavours)],
-
-"what are the timings of the shop?": [ 
-"The shop is opened from {0}! see you there! :)".format(time),],
-
-"how are you?": [ 
-"I am fine! Hope you too!:)", 
-"I am doing good!", ],
-
-"What are the prices of the cakes?": [ 
-"Umm let me check, got it ,the prices are {0}".format(price), 
-"{0}! are the prices! Pick your favourites and enjoy!!:)".format(price), 
-"Ohh just so afforable!! {0}! Enjoy!:)".format(price), ],
-
-"": [ 
-"Hey! Are you there?", 
-"What do you mean by saying nothing?", 
-"I didn't get your question :\") ",
-"Sometimes saying nothing tells a lot :)", ],
-
-"Good-bye": [
-"Have a nice day!",
-"See you again!"
+[
+        r"(.*)raining in (.*)",
+        ["No rain since last week here in %2","Damn its raining too much here in %2"]
+    ],
+    [
+        r"how (.*) health(.*)",
+        ["I'm a computer program, so I'm always healthy ",]
+    ],
+    [
+        r"(.*) (sports|game) ?",
+        ["I'm a very big fan of Football",]
+    ],
+    [
+        r"who (.*) sportsperson ?",
+        ["Messy","Ronaldo","Roony"]
 ],
+    [
+        r"who (.*) (moviestar|actor)?",
+        ["Brad Pitt"]
+],
+    [
+        r"quit",
+        ["BBye take care. See you soon :) ","It was nice talking to you. See you soon :)"]
 
-"default": [
-"this is a default message"] }
+],
+]
 
+def chatty():
+    print("Hi, I'm Chatty and I chat alot ;)\nPlease type lowercase English language to start a conversation. Type quit to leave ") #default message at the start
+    chat = Chat(pairs, reflections)
+    chat.converse()
 
-def respond(message):
-    if message in responses: 
-        bot_message = random.choice(responses[message])
-    else: 
-        bot_message = random.choice(responses["default"])
-    return bot_message
-
-
-def related(x_text): 
-  if "name" in x_text: 
-    y_text = "what's your name?"
-  elif "flavours" in x_text: 
-    y_text = "what's today's available flavours?"
-  elif "price" in x_text: 
-    y_text = "What are the prices of the cakes?"
-  elif "time" in x_text: 
-    y_text = "what are the timings of the shop?"
-  elif "how are" in x_text: 
-    y_text = "how are you?"
-  elif "bye" in x_text:
-    y_text = "Good-bye"
-  else: 
-    y_text = ""
-  return y_text
-
-
-def send_message(message): 
-  print(user_template.format(message)) 
-  response = respond(message) 
-  print(bot_template.format(response))
-
-while 1: 
-  my_input = input() 
-  my_input = my_input.lower() 
-  related_text = related(my_input) 
-  send_message(related_text)
-  if my_input == "exit" or my_input == "stop": 
-    break
+if __name__ == "__main__":
+    chatty()
